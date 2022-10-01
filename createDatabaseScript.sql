0721981146cfc8dc5d67366ed9ced4145866d7f1
JFergus98 <55389749+JFergus98@users.noreply.github.com>
2022-10-01 06:06:15 +0100

    Moved Primary key allocation in createDatabaseScript.sql
    Moved where Primary key are assigned in each statement

--- createDatabaseScript.sql
+++ createDatabaseScript.sql
@@ -21,85 +21,88 @@ USE `jamiefergusdb`;
 -- **********************************************************************
 
 CREATE TABLE platform(
-	platform_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    platform_name varchar(255) NOT NULL
+	platform_id int NOT NULL AUTO_INCREMENT,
+    platform_name varchar(255) NOT NULL,
+    PRIMARY KEY(platform_id)
 );
 
 CREATE TABLE customer(
-	customer_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	customer_id int NOT NULL AUTO_INCREMENT,
     customer_name varchar(255) NOT NULL
+    PRIMARY KEY(customer_id)
 );
 
 CREATE TABLE user_role(
-	user_role_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	user_role_id int NOT NULL AUTO_INCREMENT,
     user_role_name varchar(255) NOT NULL
+    PRIMARY KEY (user_role_id)
 );
 
 CREATE TABLE resource_type(
-	resource_type_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	resource_type_id int NOT NULL AUTO_INCREMENT,
     resource_type_name varchar(255) NOT NULL,
     platform_id int NOT NULL,
-    
+    PRIMARY KEY (resource_type_id),
     FOREIGN KEY (platform_id) REFERENCES platform(platform_id)
 );
 
 CREATE TABLE account(
-	account_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	account_id int NOT NULL AUTO_INCREMENT,
     account_ref varchar(255) NOT NULL,
     platform_id int NOT NULL,
     customer_id int NOT NULL,
-    
+    PRIMARY KEY (account_id),
     FOREIGN KEY (platform_id) REFERENCES platform(platform_id),
     FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
 );
 
 CREATE TABLE user(
-	user_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	user_id int NOT NULL AUTO_INCREMENT,
     user_name varchar(255) NOT NULL,
     customer_id int NOT NULL,
     role_id int NOT NULL,
-    
+    PRIMARY KEY (user_id),
     FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
     FOREIGN KEY (role_id) REFERENCES user_role(user_role_id)
 );
 
 CREATE TABLE resource(
-	resource_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	resource_id int NOT NULL AUTO_INCREMENT,
     resource_ref varchar(255) NOT NULL,
     account_id int NOT NULL,
     resource_type_id int NOT NULL,
     resource_name varchar(255) NOT NULL,
     last_updated varchar(255) NOT NULL,			                                    /*  *1  */
     resource_metadata longblob NOT NULL,		                                    /*  *4  */
-    
+    PRIMARY KEY (resource_id),
     FOREIGN KEY (account_id) REFERENCES account(account_id),
     FOREIGN KEY (resource_type_id) REFERENCES resource_type(resource_type_id)
 );
 
  CREATE TABLE rule(
-	rule_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	rule_id int NOT NULL AUTO_INCREMENT,
     rule_name varchar(255) NOT NULL,
     resource_type_id int NOT NULL,
-    
+    PRIMARY KEY (rule_id),
     FOREIGN KEY (resource_type_id) REFERENCES resource_type(resource_type_id)
 );
 
 CREATE TABLE non_complience_audit(
-	non_complience_audit_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	non_complience_audit_id int NOT NULL AUTO_INCREMENT,
     /*non_complience_id int NOT NULL */                                             /*  *3  */
     resource_id int NOT NULL,
     rule_id int NOT NULL,
     user_id int NOT NULL,
     action varchar(255) NOT NULL,
     action_dt varchar(255) NOT NULL,			                                    /*  *1  */
-    
+    PRIMARY KEY (non_complience_audit_id),
     FOREIGN KEY (resource_id) REFERENCES resource(resource_id),
     FOREIGN KEY (rule_id) REFERENCES rule(rule_id),
     FOREIGN KEY (user_id) REFERENCES user(user_id)
 );
 
 CREATE TABLE exception(
-	exception_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	exception_id int NOT NULL AUTO_INCREMENT,
     customer_id int NOT NULL,
     rule_id int NOT NULL,
     last_updated_by int NOT NULL,                                                   /*  *2  */
@@ -107,14 +110,14 @@ CREATE TABLE exception(
     justification varchar(255) NOT NULL,
     review_date varchar(255) NOT NULL,                                              /*  *1  */
     last_updated varchar(255) NOT NULL,                                             /*  *1  */
-    
+    PRIMARY KEY(exception_id),
     FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
     FOREIGN KEY (rule_id) REFERENCES rule(rule_id),
     FOREIGN KEY (last_updated_by) REFERENCES user(user_id)
 );
 
 CREATE TABLE exception_audit(
-	exception_audit_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	exception_audit_id int NOT NULL AUTO_INCREMENT,
     exception_id int NOT NULL,
     user_id int NOT NULL,
     customer_id int NOT NULL,
@@ -126,7 +129,7 @@ CREATE TABLE exception_audit(
     new_justification varchar(255) NOT NULL,
     old_review_time varchar(255) NOT NULL,                                          /*  *1  */
     new_review_time varchar(255) NOT NULL,                                          /*  *1  */
-    
+    PRIMARY KEY(exception_audit_id),
     FOREIGN KEY (exception_id) REFERENCES exception(exception_id),
     FOREIGN KEY (user_id) REFERENCES user(user_id),
     FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
