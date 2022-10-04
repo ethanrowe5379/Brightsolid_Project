c79fefa721908f3b8572af0944d4d6556b0cd562
Ethan Rowe <55393037+ethanrowe5379@users.noreply.github.com>
2022-10-04 12:08:14 +0100

    Updated tables to show compliant and non complaint

--- DashboardTemplate.php
+++ DashboardTemplate.php
@@ -19,96 +19,9 @@
   </header>
 
   <main>
-    <!-- <div class="container card" id="col">
-            <h2>Compliant</h2>
-            <?php
-            $sql = "SELECT t1.rule_id, t1.rule_name
-                          FROM rule t1
-                          LEFT JOIN exception_audit t2 
-                          ON t2.rule_id = t1.rule_id
-                          LEFT JOIN non_compliance_audit t3 
-                          ON t3.rule_id = t1.rule_id
-                          WHERE t2.rule_id IS NULL AND t3.rule_id IS NULL;";
-            $result = $db->query($sql);
-
-
-            if ($result->num_rows == 0) {
-              echo ("No Rules Compliant");
-            } else {
-              while ($row = $result->fetch_assoc()) {
-                echo '<div class="row card text-bg-success" id="innerCard">
-                          <h5 class="col">' . $row['rule_name'] . '</h5>
-                          <button class="col-auto btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#compliance' . $row['rule_id'] . '" aria-expanded="false" aria-controls="compliance' . $row['rule_id'] . '">
-                              Toggle Report
-                            </button>
-                            <div class="collapse" id="compliance' . $row['rule_id'] . '">
-                              <div class="card">
-                                <p>Lorem Ipsum</p>
-                              </div>
-                            </div>
-                      </div>';
-              }
-            }
-            ?>
-        </div>
-
-        <div class="container card"id="col">
-            <h2>Non-Compliant</h2>
-            <?php
-            $sql = "SELECT r.rule_id, r.rule_name FROM rule r, exception_audit ea
-                          WHERE r.rule_id = ea.rule_id";
-            $result = $db->query($sql);
-
-
-            if ($result->num_rows == 0) {
-              echo ("No Rules Compliant");
-            } else {
-              while ($row = $result->fetch_assoc()) {
-                echo '<div class="row card text-bg-success" id="innerCard">
-                            <h5 class="col">' . $row['rule_name'] . '</h5>
-                            <button class="col-auto btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#compliance' . $row['rule_id'] . '" aria-expanded="false" aria-controls="compliance' . $row['rule_id'] . '">
-                                Toggle Report
-                              </button>
-                              <div class="collapse" id="compliance' . $row['rule_id'] . '">
-                                <div class="card">
-                                  <p>Lorem Ipsum</p>
-                                </div>
-                              </div>
-                        </div>';
-              }
-            }
-            ?>
-        </div>
-
-        <div class="container card" id="col">
-            <h2>Exceptions</h2>
-            <?php
-            $sql = "SELECT r.rule_id, r.rule_name FROM rule r, non_compliance_audit nca
-                          WHERE r.rule_id = nca.rule_id";
-            $result = $db->query($sql);
-
-
-            if ($result->num_rows == 0) {
-              echo ("No Rules Compliant");
-            } else {
-              while ($row = $result->fetch_assoc()) {
-                echo '<div class="row card text-bg-success" id="innerCard">
-                            <h5 class="col">' . $row['rule_name'] . '</h5>
-                            <button class="col-auto btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#compliance' . $row['rule_id'] . '" aria-expanded="false" aria-controls="compliance' . $row['rule_id'] . '">
-                                Toggle Report
-                              </button>
-                              <div class="collapse" id="compliance' . $row['rule_id'] . '">
-                                <div class="card">
-                                  <p>Lorem Ipsum</p>
-                                </div>
-                              </div>
-                        </div>';
-              }
-            }
-            ?>
-        </div> -->
     <div class="container">
-    <table class="table table-striped table-hover">
+      <h1>Complaint</h1>
+    <table class="table table-striped table-bordered table-hover">
       <thead>
         <tr>
           <th scope="col">Rule Name</th>
@@ -118,9 +31,9 @@
       </thead>
       <tbody>
         <?php
-        $sql = "SELECT rule.rule_id, rule.rule_name, rule.rule_description, resource.resource_name FROM rule ,resource, account
-                WHERE rule.resource_type_id = resource.resource_type_id AND resource.account_id = account.account_id AND account.customer_id = 1
-                ";
+        $sql = "SELECT rule_id, rule_name, rule_description, resource_type_id FROM rule 
+        ORDER BY resource_type_id ASC;";
+
         $result = $db->query($sql);
 
 
@@ -128,12 +41,73 @@
           echo ("No Rules Compliant");
         } else {
           while ($row = $result->fetch_assoc()) {
-            echo '
-                  <tr>
+            echo '<tr>
                     <th>'. $row['rule_name'] .'</th>
                     <th>'. $row['rule_description'] .'</th>
-                    <th>' . $row['resource_name'] . '</th>
-                  </tr>
+                    <th>';
+                    
+                    $sqlResource = "SELECT * FROM resource
+                                    LEFT JOIN non_compliance 
+                                    ON resource.resource_id = non_compliance.resource_id
+                                    WHERE resource.resource_type_id = " . $row['resource_type_id'] . "
+                                    ORDER BY resource.resource_type_id ASC;";
+                    $resultResource = $db->query($sqlResource);
+
+                    while ($rowResource = $resultResource->fetch_assoc()) {
+                      echo $rowResource['resource_name'] . ", ";
+                    }
+                    echo '</th>
+                  </tr>;
+                  ';
+          }
+        }
+        ?>
+      </tbody>
+    </table>
+
+    <h1>Non-Compliant</h1>
+    <table class="table table-striped table-bordered table-hover">
+      <thead>
+        <tr>
+          <th scope="col">Rule Name</th>
+          <th scope="col">Rule Description</th>
+          <th scope="col">Resource Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        $sql = "SELECT rule_id, rule_name, rule_description, resource_type_id FROM rule 
+        ORDER BY resource_type_id ASC;";
+
+        
+
+        $result = $db->query($sql);
+
+
+        if ($result->num_rows == 0) {
+          echo ("No Rules Compliant");
+        } else {
+          while ($row = $result->fetch_assoc()) {
+            echo '<tr>
+                    <th>'. $row['rule_name'] .'</th>
+                    <th>'. $row['rule_description'] .'</th>
+                    <th>';
+                    
+                    $sqlResource = "SELECT * FROM non_compliance
+                                    LEFT JOIN resource 
+                                    ON resource.resource_id = non_compliance.resource_id
+                                    WHERE resource.resource_type_id = " . $row['resource_type_id'] . "
+                                    ORDER BY resource.resource_type_id ASC;";
+                    $resultResource = $db->query($sqlResource);
+                    if ($resultResource->num_rows == 0) {
+                      echo "No Non-Compliant Resources";
+                    } else {
+                    while ($rowResource = $resultResource->fetch_assoc()) {
+                      echo $rowResource['resource_name'] . ", ";
+                    }
+                  }
+                    echo '</th>
+                  </tr>;
                   ';
           }
         }
