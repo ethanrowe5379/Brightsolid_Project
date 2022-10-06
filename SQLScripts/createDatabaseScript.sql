d0b01331c0f860cecad528821d2db227fa43bd34
JFergus98 <55389749+JFergus98@users.noreply.github.com>
2022-10-06 15:52:11 +0100

    added resource_id to exception table

--- SQLScripts/createDatabaseScript.sql
+++ SQLScripts/createDatabaseScript.sql
@@ -116,10 +116,12 @@ CREATE TABLE exception(
     justification varchar(255) NOT NULL,
     review_date varchar(255) NOT NULL,
     last_updated varchar(255) NOT NULL,
+    resource_id int NOT NULL,
     PRIMARY KEY(exception_id),
     FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
     FOREIGN KEY (rule_id) REFERENCES rule(rule_id),
-    FOREIGN KEY (last_updated_by) REFERENCES user(user_id)
+    FOREIGN KEY (last_updated_by) REFERENCES user(user_id),
+    FOREIGN KEY (resource_id) REFERENCES resource(resource_id)
 );
 
 CREATE TABLE exception_audit(
