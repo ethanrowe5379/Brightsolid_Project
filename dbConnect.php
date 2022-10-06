05ba2b2ebdf7d86a9f5e2aa08a6bc3469a399feb
DanielJakubek <56348383+DanielJakubek@users.noreply.github.com>
2022-10-06 14:30:01 +0100

    Wrong Folder

--- dbConnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $dbc = mysqli_connect('localhost', 'root', '', 'jamiefergusdb');
-    // Check connection
-    if ($dbc -> connect_errno) {
-        echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-        exit();
-    }
-?> 
