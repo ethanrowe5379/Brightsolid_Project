9660a53a72c9e187a00516b132fee7e266c2d7da
DanielJakubek <56348383+DanielJakubek@users.noreply.github.com>
2022-10-09 16:14:22 +0100

    Fixed an issue with foreign key restraints.

--- PHP/SuspendExceptionBackEnd.php
+++ PHP/SuspendExceptionBackEnd.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    include "../connectDB.php";
+    include "dbConnect.php";
 
     //If fields are not empty
     if(isset($_POST['suspendExceptionID']) && isset($_POST['suspendExceptionValue']) )
@@ -12,14 +12,19 @@
         $lastUpdatedBy = $_SESSION['userID'];
         $customerID = $_SESSION['customerID'];
 
+        $disableForeignKeyCheck = "SET FOREIGN_KEY_CHECKS=0;";
+        $enableForeignKeyCheck = "SET FOREIGN_KEY_CHECKS=1;";
 
         $sqlQuery = "DELETE FROM exception WHERE exception_id = ". $exceptionID ." AND exception_value = '". $exceptionValue ."';";
+    
+        mysqli_query($dbc, $disableForeignKeyCheck);
         $result = mysqli_query($dbc, $sqlQuery);
+        mysqli_query($dbc, $enableForeignKeyCheck);
 
         header("Location: ../ManagerDashboard.php");
     }
     else{
-        header("Location: Index.php"); ////ADD redirect to respective page via switch
+        header("Location: ../ManagerDashboard.php"); ////ADD redirect to respective page via switch
     }
     
 ?>
